f9ecebd1c6d27606631c082491053c750e16fc6c
David Margoses <davidmargoses@gmail.com>
2021-07-31 13:47:32 +0200

    build Home page

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Placeholder from '../assets/placeholder.png';
+import { useHistory } from 'react-router-dom';
 
 const Home = () => {
-    return <div>Hello Home</div>;
+    const history = useHistory();
+    return (
+        <div className="home-container">
+            <div className="home-header">
+                <h2>Popular Titles</h2>
+            </div>
+            <div className="home-content">
+                <div className="home-series">
+                    <button onClick={() => history.push('/series')}>
+                        <img src={Placeholder} alt="placeholder" />
+                        <h3>SERIES</h3>
+                    </button>
+                    <span>Popular series</span>
+                </div>
+                <div className="home-movies">
+                    <button onClick={() => history.push('/movies')}>
+                        <img src={Placeholder} alt="placeholder" />
+                        <h3>MOVIES</h3>
+                    </button>
+                    <span>Popular movies</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
