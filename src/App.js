d45d885b772e5e1d86e4f55a0526e290bceface9
David Margoses <davidmargoses@gmail.com>
2021-07-31 08:17:43 +0200

    squelet app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Containers/Home';
+import MoviesSeries from './Containers/MoviesSeries';
 const App = () => {
-    return <div>Ensembl Challenge</div>;
+    return (
+        <Router>
+            <Switch>
+                <Route path="/data" component={MoviesSeries} />
+                <Route path="/" component={Home} />
+            </Switch>
+        </Router>
+    );
 };
 
 export default App;
---
+++ src/Containers/MoviesSeries.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MoviesSeries = () => {
+    return <div>Hello MoviesSeries</div>;
+};
+
+export default MoviesSeries;
